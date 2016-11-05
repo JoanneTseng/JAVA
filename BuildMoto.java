bfeecab4c49a9d4a5a9bb7d9353c63adbb1cc6ea
JoanneTseng <M10209213@mail.ntust.edu.tw>
2016-11-05 18:47:10 +0800

    Update BuildMoto.java

--- BuildMoto.java
+++ BuildMoto.java
@@ -5,8 +5,8 @@ class Moto
   
   void printState()//void表示此方法無傳回值。顯示物件狀態的方法。此類別的一個方法。
   {
-  System.out.print("目前載油量為"+gas+"公升，"); 
-  System.out.println("耗油率為每公升跑"+eff+"公里");
+    System.out.print("目前載油量為"+gas+"公升，"); 
+    System.out.println("耗油率為每公升跑"+eff+"公里");
   
   }
 }
@@ -15,11 +15,11 @@ public class BuildMoto
 {
   public static void main(String[] argv)
   {
-  Moto oldmoto = new Moto();//建立物件
+    Moto oldmoto = new Moto();//建立物件
   
-  oldmoto.gas = 10;//設定成員變數值
-  oldmoto.eff = 1;
-  System.out.print("舊機車oldmoto");
-  oldmoto.printState();//呼叫方法
+    oldmoto.gas = 10;//設定成員變數值
+    oldmoto.eff = 1;
+    System.out.print("舊機車oldmoto");
+    oldmoto.printState();//呼叫方法
   }
 }
